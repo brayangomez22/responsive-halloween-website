db4d27c0241e2d715685d4019fe5b51da764c762
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-10-23 20:28:14 -0500

    The latest developments are finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,25 @@ function scrollActive() {
 	});
 }
 window.addEventListener('scroll', scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+	const scrollUp = document.getElementById('scroll-up');
+	if (this.scrollY >= 460) scrollUp.classList.add('show-scroll');
+	else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+	origin: 'top',
+	distance: '60px',
+	duration: 2500,
+	delay: 400,
+	// reset: true
+});
+
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`);
+sr.reveal(`.category__data, .trick__content, .footer__content`, { interval: 100 });
+sr.reveal(`.about__data, .discount__img`, { origin: 'left' });
+sr.reveal(`.about__img, .discount__data`, { origin: 'right' });
