9e8818c9dbc5532d27023819c0fe16b7bcf95ff8
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-10-23 19:50:36 -0500

    The New Arrivals section of the main was developed

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,31 @@ function scrollHeader() {
 	else header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
+
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper('.new-swiper', {
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	loop: 'true',
+	spaceBetween: 16,
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+	const scrollY = window.pageYOffset;
+
+	sections.forEach((current) => {
+		const sectionHeight = current.offsetHeight,
+			sectionTop = current.offsetTop - 58,
+			sectionId = current.getAttribute('id');
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+		} else {
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+		}
+	});
+}
+window.addEventListener('scroll', scrollActive);
