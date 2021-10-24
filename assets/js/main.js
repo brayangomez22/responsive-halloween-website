14ab4a7205626d5e179f03bf8826d465291f1e51
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-10-23 19:30:20 -0500

    The section category of the main was developed

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ const navMenu = document.getElementById('nav-menu'),
 	navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
 if (navToggle) {
 	navToggle.addEventListener('click', () => {
 		navMenu.classList.add('show-menu');
@@ -12,7 +11,6 @@ if (navToggle) {
 }
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 if (navClose) {
 	navClose.addEventListener('click', () => {
 		navMenu.classList.remove('show-menu');
@@ -24,7 +22,6 @@ const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
 	const navMenu = document.getElementById('nav-menu');
-	// When we click on each nav__link, we remove the show-menu class
 	navMenu.classList.remove('show-menu');
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction));
@@ -39,3 +36,11 @@ let homeSwiper = new Swiper('.home-swiper', {
 		clickable: true,
 	},
 });
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+	const header = document.getElementById('header');
+	if (this.scrollY >= 50) header.classList.add('scroll-header');
+	else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
